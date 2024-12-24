629876adb121cf78272ae38f03a8228cac5160fe
Agastyasourcecodes <139317735+Agastyasourcecodes@users.noreply.github.com>
2024-12-24 14:39:40 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 24, 2024',
+  birthdate: 'Dec 25, 2024',
   name: 'Karan'
 };
 
